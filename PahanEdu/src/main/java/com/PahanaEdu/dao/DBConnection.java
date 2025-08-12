2655d53fa5a7d161c9e15289fbe81f9ad123c2de
FixHome <FixHome@Charuka>
2025-08-12 10:54:42 +0530

    database connection

---
+++ PahanEdu/src/main/java/com/PahanaEdu/dao/DBConnection.java
@@ -0,0 +1,40 @@
+package com.PahanaEdu.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+public class DBConnection {
+	
+	private static final String DB_URL = "jdbc:mysql://localhost:3306/?user=root/pahanaedu";
+	private static final String DB_USER = "root";
+	private static final String DB_PASSWORD = "Charuka98";
+
+	private static DBConnection instance;
+	private Connection connection;
+
+	private DBConnection() {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			this.connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+		} catch (ClassNotFoundException | SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static DBConnection getInstance() {
+		if (instance == null) {
+			synchronized (DBConnection.class) {
+				if (instance == null) {
+					instance = new DBConnection();
+				}
+			}
+		}
+		return instance;
+	}
+
+	public Connection getConnection() {
+		return connection;
+	}
+}
---
+++ PahanEdu/src/main/java/com/PahanaEdu/dao/DBConnectionFactory.java
@@ -0,0 +1,12 @@
+package com.PahanaEdu.dao;
+
+import java.sql.Connection;
+
+
+public class DBConnectionFactory {
+	
+	 public static Connection getConnection() {
+	        return DBConnection.getInstance().getConnection();
+	    }
+
+}
